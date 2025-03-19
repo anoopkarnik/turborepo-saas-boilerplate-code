fb76b6c04801d8d106545b3356b2e59c5ea4eec5
anoopkarnik <danoopkarnik@gmail.com>
2025-03-20 03:14:53 +0530

    Expanded Support Component to have tabs for chat, feedback, resources and social

--- packages/ui/src/components/organisms/custom/home/SupportChat.tsx
+++ packages/ui/src/components/organisms/custom/home/SupportChat.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '../../../atoms/shadcn/button';
 import { Textarea } from '../../../atoms/shadcn/textarea';
 import { chatWithAssistant} from "../../../../../../ai/src/openai/assistant";
@@ -31,83 +31,64 @@ export const SupportChat = () => {
       .replace(/\n/g, "<br>"); // Line breaks
   };
 
-  const [isChatOpen, setIsChatOpen] = useState(false); // State to control chat visibility
-
   const {data:session} = useSession()
 
   const handleChatOpening = async () => {
-    setIsChatOpen(true);
     if(firstMessage){
       await sendMessage();
       setFirstMessage(false);
     }
-    setIsChatOpen(true);
   }
+
+  useEffect(() => {
+    handleChatOpening();
+  }, []);
 
 
   return (
-    <div className="fixed bottom-4 right-4">
-      {!isChatOpen && (
-        <button
-          onClick={handleChatOpening}
-          className="bg-primary text-background rounded-full p-4 shadow-lg"
-        >
-          <ChatBubbleIcon className='w-8 h-8'/> 
-        </button>
-      )}
-      {isChatOpen && (
-        <div className="fixed bottom-10 right-4 bg-sidebar shadow-lg rounded-lg w-[40%] border">
-          <div className='flex flex-col'>
-            <div className="flex justify-between items-center p-4 ">
-              <h3 className="font-semibold">Support Chat</h3>
-              <button
-                onClick={() => setIsChatOpen(false)}
-                className="text-gray-500 hover:text-gray-700"
-              >
-                ✖
-              </button>
-            </div>
-            <div className="bg-sidebar text-foreground h-[500px] border-[1px] overflow-y-auto p-4 flex flex-col gap-2 text-paragraph">
-              {messages.slice(1).map((message, index) => (
-                <div
-                  key={index}
-                  className={`p-2 rounded-lg max-w-[80%] flex items-start gap-2 ${
-                    message.role === "user"
-                      ? "bg-sidebar-accent text-foreground self-start"
-                      : "bg-primary text-background self-end"
-                  }`}
-                >
-                  {message.role == "user" && <Image src={session?.user?.image as string} alt='U' width={30} height={30} className='rounded-full' />}
-                  {message.role == "assistant" && <Image src='/connections/openai-black.png' alt='U' width={30} height={30} className='rounded-full' />}
-                  <div dangerouslySetInnerHTML={{
-                    __html: message.role === "assistant" ? formatMessage(message.value) : message.value,}} 
-                    className="py-1 pr-2 rounded-lg max-w-full"></div>
-                </div>
-              ))}
-              <div className='text-description self-end'>
-                {waitingForReply && 
-                <div className='flex items-center gap-4'>
-                  <span>Assistant is typing</span>
-                  <Skeleton className='w-[180px] h-[30px] rounded-lg bg-primary text-primary'/>
-                </div>
-                }
-              </div>
-            </div>
-            <div role="button" tabIndex={0} className='flex items-center ' onKeyPress={(e) => e.key === 'Enter' && sendMessage()}>
-              <Textarea
-                className='flex-1 bg-sidebar text-sidebar-foreground border-none rounded-none rounded-bl-lg min-h-[60px] max-h-[60px] focus-visible:ring-offset-0 focus-visible:ring-0 '
-                  value={userInput}
-                  onChange={(e) => setUserInput(e.target.value)}
-                  placeholder="Type your message..."
-              />
-              <Button onClick={sendMessage} 
-              className='m-0 p-6 rounded-none rounded-br-lg border-0 min-h-[60px] max-h-[60px]'>
-                Send
-              </Button>
-            </div>
+    <div className='flex flex-col'>
+      <div className="flex justify-between items-center">
+        <h3 className="font-semibold">Support Chat</h3>
+      </div>
+      <div className=" text-foreground h-[500px]  overflow-y-auto p-4 flex flex-col gap-2 text-paragraph scrollbar scrollbar-track-secondary scrollbar-thumb-sidebar">
+        {messages.slice(1).map((message, index) => (
+          <div
+            key={index}
+            className={`p-2 rounded-lg max-w-[80%] flex items-start gap-2 ${
+              message.role === "user"
+                ? "bg-sidebar-accent text-foreground self-start"
+                : "bg-primary text-background self-end"
+            }`}
+          >
+            {message.role == "user" && <Image src={session?.user?.image as string} alt='U' width={30} height={30} className='rounded-full' />}
+            {message.role == "assistant" && <Image src='/connections/openai-black.png' alt='U' width={30} height={30} className='rounded-full' />}
+            <div dangerouslySetInnerHTML={{
+              __html: message.role === "assistant" ? formatMessage(message.value) : message.value,}} 
+              className="py-1 pr-2 rounded-lg max-w-full"></div>
           </div>
+        ))}
+        <div className='text-description self-end'>
+          {waitingForReply && 
+          <div className='flex items-center gap-4'>
+            <span>Assistant is typing</span>
+            <Skeleton className='w-[180px] h-[30px] rounded-lg bg-primary text-primary'/>
+          </div>
+          }
         </div>
-       )}
+      </div>
+      <div role="button" tabIndex={0} className='flex items-center ' onKeyPress={(e) => e.key === 'Enter' && sendMessage()}>
+        <Textarea
+          className='flex-1 bg-background border-none rounded-none rounded-bl-lg min-h-[60px] max-h-[60px] focus-visible:ring-offset-0 focus-visible:ring-0 '
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            placeholder="Type your message..."
+        />
+        <Button onClick={sendMessage} 
+        className='m-0 p-6 rounded-none rounded-br-lg border-0 min-h-[60px] max-h-[60px]'>
+          Send
+        </Button>
+      </div>
     </div>
+
   );
 };
--- packages/ui/src/components/organisms/custom/landing/v1/Support.tsx
+++ packages/ui/src/components/organisms/custom/landing/v1/Support.tsx
@@ -1,115 +1,142 @@
-"use client"
-import React, { useEffect, useState, useRef } from 'react';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../../../../molecules/shadcn/dropdown';
-import { MessageCircleQuestion } from 'lucide-react';
-import { DropdownMenuGroup, DropdownMenuLabel, DropdownMenuSeparator } from '@radix-ui/react-dropdown-menu';
-import { useRouter } from 'next/navigation';
-import { Separator } from '../../../../atoms/shadcn/separator';
-import { HeroSectionProps } from '@repo/ts-types/landing-page/hero';
-import { FooterSectionProps } from '@repo/ts-types/landing-page/footer';
-import { NavbarSectionProps } from '@repo/ts-types/landing-page/navbar';
-import { Input } from '../../../../atoms/shadcn/input';
-import { Textarea } from '../../../../atoms/shadcn/textarea';
-import { Button } from '../../../../atoms/shadcn/button';
-import {sendSupportEmail} from '@repo/email/resend/index'
+"use client";
+import React, { useEffect, useState, useRef } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../../../../molecules/shadcn/dropdown";
+import { MessageCircleQuestion } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { HeroSectionProps } from "@repo/ts-types/landing-page/hero";
+import { FooterSectionProps } from "@repo/ts-types/landing-page/footer";
+import { NavbarSectionProps } from "@repo/ts-types/landing-page/navbar";
+import { Input } from "../../../../atoms/shadcn/input";
+import { Textarea } from "../../../../atoms/shadcn/textarea";
+import { Button } from "../../../../atoms/shadcn/button";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "../../../../molecules/shadcn/tabs";
+import { SupportChat } from "../../home/SupportChat";
 
-const Support = ({ heroSection, footerSection, navbarSection}: {
-     heroSection: HeroSectionProps, footerSection: FooterSectionProps, navbarSection: NavbarSectionProps}) => {
+const Support = ({
+  heroSection,
+  footerSection,
+  navbarSection,
+}: {
+  heroSection: HeroSectionProps;
+  footerSection: FooterSectionProps;
+  navbarSection: NavbarSectionProps;
+}) => {
   const [open, setOpen] = useState(false);
+  const [selectedTab, setSelectedTab] = useState("feedback"); // Default Tab
   const triggerRef = useRef<HTMLDivElement>(null);
-  const [subject, setSubject] = useState<string>('');
-  const [message, setMessage] = useState<string>('');
+  const [subject, setSubject] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
 
   const handleOpenChange = (isOpen: boolean) => {
     setOpen(isOpen);
   };
 
-  useEffect(() => {
-  },[heroSection,footerSection])
+  useEffect(() => {}, [heroSection, footerSection]);
 
   const router = useRouter();
 
   const handleSendFeedback = async () => {
-    if(message){
-        const response = await fetch('/api/email/sendFeedback', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ subject, message }),
-        });
-        console.log(response);
-        const result = await response.json();
-        console.log(result);
-        setSubject('');
-        setMessage('');
-        }
+    if (message) {
+      const response = await fetch("/api/email/sendFeedback", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ subject, message }),
+      });
+      console.log(response);
+      const result = await response.json();
+      console.log(result);
+      setSubject("");
+      setMessage("");
     }
+  };
 
   return (
     <DropdownMenu open={open} onOpenChange={handleOpenChange}>
-        <DropdownMenuTrigger asChild>
-            <div
-            ref={triggerRef}
-            className='fixed bottom-4 right-4 flex items-center justify-center gap-2 p-4 rounded-full 
-            bg-sidebar cursor-pointer opacity-80 hover:opacity-100'
-            >
-            <MessageCircleQuestion className='w-6 h-6 text-primary' />
+      <DropdownMenuTrigger asChild>
+        <div
+          ref={triggerRef}
+          className="fixed bottom-4 right-6 flex items-center justify-center gap-2 p-4 rounded-full bg-sidebar-foreground/30s dark:bg-sidebar cursor-pointer opacity-80 hover:opacity-100"
+        >
+          <MessageCircleQuestion className="w-6 h-6 text-primary" />
+        </div>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-[600px] px-4 max-h-screen pb-20" onCloseAutoFocus={(e) => e.preventDefault()}>
+        <Tabs defaultValue="feedback" className="w-full">
+          {/* 📌 Content Sections */}
+          <TabsContent value="feedback">
+            <div className="flex flex-col gap-2">
+              <Input
+                placeholder="Subject"
+                className="w-full bg-background"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                onKeyDown={(e) => e.stopPropagation()} // Prevent dropdown navigation
+              />
+              <Textarea
+                placeholder="Message (Required)"
+                className="w-full h-[200px]"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyDown={(e) => e.stopPropagation()} // Prevent dropdown navigation
+              />
+              <Button size={"sm"} className="mb-2" onClick={handleSendFeedback}>
+                Send Feedback
+              </Button>
             </div>
-        </DropdownMenuTrigger >
-        <DropdownMenuContent className='w-[400px] px-4' onCloseAutoFocus={(e) => e.preventDefault()} >
-                {/* 🌎 Join Us */}
-                <DropdownMenuGroup>
-                    <DropdownMenuLabel className='text-description'>Check Out</DropdownMenuLabel>
-                    {footerSection?.footerList?.filter(item=> item.type === "Follow Us").map((item, index) => (
-                            <DropdownMenuItem key={index} className='cursor-pointer text-xs' 
-                            onClick={() => window.open(item.href, '_blank')}>
-                                {item.label}
-                            </DropdownMenuItem>
-                        ))
-                    }
-                </DropdownMenuGroup>
+          </TabsContent>
 
-                <Separator className='my-2' />
-                {/* 📖 Resources */}
-                {(heroSection.documentationLink && heroSection.blogLink) && 
-                <DropdownMenuGroup>
-                    <DropdownMenuLabel className='text-description'>Resources</DropdownMenuLabel>
+          <TabsContent value="resources">
+            <div className="flex flex-col gap-2">
+                <p className="text-description text-sm">For Details:</p>
 
-                    {heroSection?.documentationLink && <DropdownMenuItem className='cursor-pointer text-xs'
-                     onClick={() => router.push(heroSection.documentationLink as string)}>
-                        Documentation
-                    </DropdownMenuItem>}
-                    {heroSection?.blogLink && <DropdownMenuItem className='cursor-pointer text-xs' 
+                    {heroSection?.documentationLink && 
+                    <DropdownMenuItem className='cursor-pointer text-sm'
+                        onClick={() => router.push(heroSection.documentationLink as string)}>
+                            Documentation
+                        </DropdownMenuItem>}
+                    {heroSection?.blogLink && 
+                    <DropdownMenuItem className='cursor-pointer text-sm' 
                     onClick={() => router.push(heroSection.blogLink as string)}>
                         Blog
                     </DropdownMenuItem>}
-                </DropdownMenuGroup>}
-                <Separator className='mb-2' />
-                {/* 🛠 Feedback*/}
-                <DropdownMenuGroup>
-                    <DropdownMenuLabel className='text-description mb-2'>Feeback</DropdownMenuLabel>
-                    <div className='flex flex-col gap-2'>
-                        <Input 
-                            placeholder='Subject' 
-                            className='w-full bg-background'
-                            value={subject}
-                            onChange={(e)=>setSubject(e.target.value)}
-                            onKeyDown={(e) => e.stopPropagation()} // Prevent dropdown navigation
-                        />
-                        <Textarea 
-                            placeholder='Message (Required)' 
-                            className='w-full'
-                            value={message}
-                            onChange={(e)=>setMessage(e.target.value)}
-                            onKeyDown={(e) => e.stopPropagation()} // Prevent dropdown navigation
-                        />
-                        <Button size={'sm'} className='mb-2' onClick={handleSendFeedback}>Send Feedback</Button>
-                    </div>
-                </DropdownMenuGroup>
+            </div>
+          </TabsContent>
+          <TabsContent value="supportbot">
+            <SupportChat />
+          </TabsContent>
+          <TabsContent value="social">
+            <div className="flex flex-col gap-2">
+              <p className="text-description text-sm">Follow us on:</p>
+              {footerSection?.footerList
+                ?.filter((item) => item.type === "Follow Us")
+                .map((item, index) => (
+                
+                    <DropdownMenuItem className='cursor-pointer text-sm'
+                    onClick={() => window.open(item.href, "_blank")}>
+                        {item.label}
+                        </DropdownMenuItem>
 
-        </DropdownMenuContent>
+                ))}
+            </div>
+          </TabsContent>
+
+          {/* 🔥 Tabs at the Bottom */}
+          <TabsList className="absolute bottom-0 left-0 w-full  p-2 rounded-b-lg flex justify-around">
+            <TabsTrigger value="feedback">Feedback</TabsTrigger>
+            <TabsTrigger value="resources">Resources</TabsTrigger>
+            <TabsTrigger value="supportbot">Support Bot</TabsTrigger>
+            <TabsTrigger value="social">Social</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </DropdownMenuContent>
     </DropdownMenu>
-
-     
   );
 };
 
